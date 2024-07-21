1607b1d2d96fa2ab2d9a0bd082b2e155f753338f
wjuma19dev <wjuma19dev@gmail.com>
2024-07-21 09:00:36 -0400

    Started git on project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './components/button/button.component';
+import { ControlComponent } from "./components/control/control.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ButtonComponent, ControlComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'template-deep-dive';
+
+  // @ViewChild('form', { static: true }) private form!: ElementRef<HTMLFormElement>;
+
+  // REVIEW ViewChild Signals
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+
+  onSubmit() {
+    // console.log({ 'title': title.value, 'description': description.value })
+    // console.log('Form: ', this.form)
+    this.form().nativeElement.reset();
+    // console.log(this.form().nativeElement)
+  }
 }
---
+++ src/app/components/control/control.component.ts
@@ -0,0 +1,25 @@
+import { Component, contentChild, ContentChild, ElementRef, inject, input } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css'
+})
+export class ControlComponent {
+
+  public label = input.required<string>();
+
+  private el: ElementRef<HTMLDivElement> = inject(ElementRef);
+
+  // @ContentChild('input') input!: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+
+  private input = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+  onClick() {
+    console.log(this.input()?.nativeElement)
+    console.log(this.el.nativeElement)
+  }
+
+}
